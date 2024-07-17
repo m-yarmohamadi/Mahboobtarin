c3d2c82f7c5dfb8b228a9f340c43b66c47e6ba9f
m-yarmohamadi <yarmohamadi677@gmail.com>
2024-07-17 08:13:39 +0330

    edit phone number

--- pages/users/login.js
+++ pages/users/login.js
@@ -1,15 +1,36 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import React from 'react';
+import React, { useState } from 'react';
+import { BiFontSize } from 'react-icons/bi';
+import { PhoneInput } from 'react-international-phone';
+import 'react-international-phone/style.css';
 
 const login = () => {
-	return (
-		<div>
-			<Header />
-			<div className=' container w-full h-96 bg-primary-02'>LOGIN</div>
-			<Footer />
-		</div>
-	);
+  const [phone, setPhone] = useState('');
+
+  return (
+    <div className='mt-20'>
+      <Header />
+      <div className='px-10 container flex flex-col justify-center items-center w-full h-96 bg-primary-01 bg-opacity-10'>
+        <div className='w-full container text-lg font-bold mx-auto flex justify-center items-center py-5'>
+          <span>لطفا جهت ورود به وبسایت و استفاده از امکانات بیشتر شماره تلفن همراه خود را وارد کنید.</span>
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+          <PhoneInput
+            defaultCountry='ir'
+            value={phone}
+            onChange={(phone) => setPhone(phone)}
+            inputStyle={{ fontSize: '26px', width: '250px', height:'50px' }}
+			style={{height:'50px'}}
+			sele
+          />
+        </div>
+        {phone}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default login;
