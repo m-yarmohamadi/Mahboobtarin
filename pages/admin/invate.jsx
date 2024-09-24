2de1415277ad3ab2a759b63afcbecd64956ea60a
Mohammad Reza Faramarzi <mf575583@gmail.com>
2024-09-24 15:54:03 +0330

    fix dashboard

---
+++ pages/admin/invate.jsx
@@ -0,0 +1,13 @@
+import ExpertDashboard from "@/components/admin/ExpertDashboard";
+
+export default function invate() {
+    return (
+        <ExpertDashboard>
+            <div className="w-full flex items-center justify-between border-b border-b-gray-300 pb-4 mb-4">
+                <h1 className="text-xl text-gray-800 font-semibold">
+                    دعوت از دوستان
+                </h1>
+            </div>
+        </ExpertDashboard>
+    )
+}
