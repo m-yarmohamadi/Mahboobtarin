19a7752747312fab0cf7bf583688180caa5f26db
Mohammad Reza Faramarzi <mf575583@gmail.com>
2024-10-31 17:16:34 +0330

    fix category

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -2,16 +2,18 @@ import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import { FaAngleLeft } from "react-icons/fa";
 import Loading from "@/tools/Loading";
-import { useCategoryChild } from "@/hooks/useMainPage";
 
 export default function Categories({ isOpen, setIsOpen, categories, isLoading }) {
     const [lastHover, setLastHover] = useState(0);
-    // const { categoryChilds, isGetCateChild } = useCategoryChild(lastHover);
     const categoryListRef = useRef(null);
     const [childrenRecursive, setChildrenRecursive] = useState({});
 
     useEffect(() => {
-        if (!isLoading) setChildrenRecursive(categories[0] || {});
+        if (!isLoading) {
+            const firstCategory = categories.filter((c) => c.parent_id === 0)[0];
+            setChildrenRecursive(firstCategory || {});
+            setLastHover(firstCategory.id)
+        }
     }, [isLoading]);
 
     useEffect(() => {
@@ -64,43 +66,46 @@ export default function Categories({ isOpen, setIsOpen, categories, isLoading })
                             ))}
                     </ul>
 
-                    <div className={`flex-1 flex min-h-full p-7 z-50 bg-white overflow-y-auto drop-shadow-lg dark:shadow-darkLg`}>
+                    <div className={`flex-1 flex min-h-full pt-7 pr-7 z-50 bg-white overflow-y-auto drop-shadow-lg dark:shadow-darkLg`}>
                         {!isLoading ?
-                            <div className="flex-grow flex-1 h-full flex flex-col">
+                            <div className="w-full flex flex-col flex-1 flex-grow">
                                 <Link
                                     href={`/group/${childrenRecursive?.id}`}
-                                    className=" text-xs mb-5 font-medium text-secondary-01 inline-flex items-center gap-2 !p-0"
+                                    className=" text-xs mb-7 font-medium text-secondary-01 inline-flex items-center gap-2 !p-0 whitespace-nowrap"
                                 >
                                     همه موارد مرتبط با {childrenRecursive?.name}
                                     <FaAngleLeft className="w-3 h-3" />
                                 </Link>
-                                <ul className="h-auto min-h-[400px] flex-1 grid grid-cols-4 auto-rows-min">
+                                <ul className={`[column-width:180px]  [column-fill:balance]`}>
                                     {childrenRecursive?.children_recursive?.map(
                                         (subItem) => (
-                                            <React.Fragment key={subItem.id}>
-                                                <li className="w-auto">
+                                            <div key={subItem.id} className="break-inside-avoid">
+                                                <li className="pt-1 pb-2 pl-6">
                                                     <Link
                                                         href={`/group/${subItem.id}`}
-                                                        className="text-slate-800 flex items-center border-r-2 pr-2 mb-1 mt-4 border-r-primary-01 font-semibold text-xs  cursor-pointer duration-200"
+                                                        className="text-slate-800 flex items-center border-r-2 pr-1 border-r-primary-01 font-semibold text-xs  cursor-pointer duration-200"
                                                     >
                                                         {subItem.name}
                                                         <FaAngleLeft className="w-3 h-3" />
                                                     </Link>
                                                 </li>
-                                                {subItem?.children_recursive?.map((subMenu) => (
-                                                    <li key={subMenu.id} className="mb-1 w-auto pr-2">
-                                                        <Link
-                                                            href={`/group/${subItem.id}`}
-                                                            className="text-slate-500  text-[10px] cursor-pointer duration-200"
-                                                        >
-                                                            {subMenu.name}
-                                                        </Link>
-                                                    </li>
-                                                ))}
-                                            </React.Fragment>
+                                                <ul className="pl-10 mb-4">
+                                                    {subItem?.children_recursive?.map((subMenu) => (
+                                                        <li key={subMenu.id}>
+                                                            <Link
+                                                                href={`/group/${subItem.id}`}
+                                                                className="text-slate-500  text-[10px] cursor-pointer duration-200"
+                                                            >
+                                                                {subMenu.name}
+                                                            </Link>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
                                         )
                                     )}
                                 </ul>
+
                             </div>
                             :
                             <div className="w-full min-h-full flex items-center justify-center">
