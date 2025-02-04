bceaa45f56a8beea5ec4e2df482d47c0904a920a
Mohammad Reza Faramarzi <mf575583@gmail.com>
2025-02-04 13:39:27 +0330

    feat: why_us api

--- components/Resume.js
+++ components/Resume.js
@@ -4,8 +4,10 @@ import { PiBagBold } from "react-icons/pi";
 import { GiMaterialsScience } from "react-icons/gi";
 import { TbSettingsCheck } from "react-icons/tb";
 import { MdOutlineForum } from "react-icons/md";
+import useMainPage from "@/hooks/useMainPage";
 
 const Resume = () => {
+  const { why_us, isLoading } = useMainPage();
   return (
     <div className="py-8 bg-gradient-to-b from-primary-02 to-white">
       <div className=" md:container px-8 md:px-0 ">
@@ -13,82 +15,27 @@ const Resume = () => {
           چرا محبوب‌ترین؟
         </div>
         <div className="grid xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-16 items-start justify-items-center text-4xl text-primary-01">
-          <div className="w-full flex flex-col justify-center items-center">
-            <div className="w-full flex justify-center items-center">
-              <div className=" border border-primary-01 p-4 rounded-3xl w-fit">
-                <LiaCertificateSolid />
+          {why_us?.map((item, index) => (
+            <div
+              key={index}
+              className="w-full flex flex-col justify-center items-center"
+            >
+              <div className="w-full flex justify-center items-center">
+                <div
+                  dangerouslySetInnerHTML={{ __html: item.image }}
+                  className=" border border-primary-01 p-4 rounded-3xl w-fit"
+                ></div>
+              </div>
+              <div className="flex flex-col justify-center items-center justify-items-center">
+                <span className="whitespace-nowrap py-3 font-bold text-xl text-slate-800">
+                  {item.title}
+                </span>
+                <p className="text-sm text-center text-slate-900">
+                  {item.description}
+                </p>
               </div>
             </div>
-            <div className="flex flex-col justify-center items-center justify-items-center">
-              <span className="whitespace-nowrap py-3 font-bold text-xl text-slate-800">
-                دانشنامه چند منظوره
-              </span>
-              <p className="text-sm text-center text-slate-900">
-                شناساندن مفاخر و مشاهیر و تسهیل دسترسی به آنان
-              </p>
-            </div>
-          </div>
-          <div className="w-full flex flex-col justify-center items-center">
-            <div className="w-full flex justify-center items-center">
-              <div className=" border border-primary-01 p-4 rounded-3xl w-fit">
-                <PiBagBold />
-              </div>
-            </div>
-            <div className="flex flex-col justify-center items-center justify-items-center">
-              <span className="whitespace-nowrap py-3 font-bold text-xl text-slate-800">
-                تسهیل استخدام
-              </span>
-              <p className="text-sm text-center text-slate-900">
-                شانس استخدام بیشتر برای فارغ التحصیلان دوره ها در سایت های
-                کاریابی
-              </p>
-            </div>
-          </div>
-          <div className="w-full flex flex-col justify-center items-center">
-            <div className="w-full flex justify-center items-center">
-              <div className=" border border-primary-01 p-4 rounded-3xl w-fit">
-                <GiMaterialsScience />
-              </div>
-            </div>
-            <div className="flex flex-col justify-center items-center justify-items-center">
-              <span className="whitespace-nowrap py-3 font-bold text-xl text-slate-800">
-                راهنمای علمی
-              </span>
-              <p className="text-sm text-center text-slate-900">
-                پاسخ گویی به سوالات و راهنمایی توسط استاد یا منتور دوره
-              </p>
-            </div>
-          </div>
-          <div className="w-full flex flex-col justify-center items-center">
-            <div className="w-full flex justify-center items-center">
-              <div className=" border border-primary-01 p-4 rounded-3xl w-fit">
-                <TbSettingsCheck />
-              </div>
-            </div>
-            <div className="flex flex-col justify-center items-center justify-items-center">
-              <span className="whitespace-nowrap py-3 font-bold text-xl text-slate-800">
-                پروژه محور
-              </span>
-              <p className="text-sm text-center text-slate-900">
-                دوره های پروزه محور برای کسب مهارت انجام پروژه های واقعی
-              </p>
-            </div>
-          </div>
-          <div className="w-full flex flex-col justify-center items-center">
-            <div className="w-full flex justify-center items-center">
-              <div className=" border border-primary-01 p-4 rounded-3xl w-fit">
-                <MdOutlineForum />
-              </div>
-            </div>
-            <div className="flex flex-col justify-center items-center justify-items-center">
-              <span className="whitespace-nowrap py-3 font-bold text-xl text-slate-800">
-                تالار گفتگو
-              </span>
-              <p className="text-sm text-center text-slate-900">
-                امکان برقراری ارتباط با دانشجویان و پرسش سوالات
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
