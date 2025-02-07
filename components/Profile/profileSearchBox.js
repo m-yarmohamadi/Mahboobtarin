84483865632f1ba776613e33a04af4cccfb19baf
Mohammad Reza Faramarzi <mf575583@gmail.com>
2025-02-07 20:17:52 +0330

    fix bugs

--- components/Profile/profileSearchBox.js
+++ components/Profile/profileSearchBox.js
@@ -14,30 +14,28 @@ const ProfileSearchBox = ({ user }) => {
   );
 
   return (
-    <div className="">
-      <div className="w-full container  bg-primary-01 md:rounded-b-3xl">
-        <div className="text-white w-full flex flex-col justify-between items-start pb-48 gap-6">
-          <div
-            className={`w-full flex flex-col xs:flex-row pt-4 gap-4 items-center justify-between duration-100 ${
-              isGetCateParent ? "opacity-40 blur-sm" : ""
-            }`}
-          >
-            <div className="url-params-profile-page flex justify-center items-center gap-1 text-xs md:text-sm group">
-              {!isGetCateParent && categoryParents && (
-                <UrlItem data={categoryParents} />
-              )}
-            </div>
-            <div className="flex justify-center items-center ">
-              <Platform
-                data={user?.socialmedia}
-                color={`text-primary-03`}
-                colorHover={`text-white`}
-              />
-            </div>
+    <div className="w-full container  bg-primary-01 md:rounded-b-3xl">
+      <div className="text-white w-full flex flex-col justify-between items-start pb-48 gap-6">
+        <div
+          className={`w-full flex flex-col xs:flex-row pt-4 gap-4 items-center justify-between duration-100 ${
+            isGetCateParent ? "opacity-40 blur-sm" : ""
+          }`}
+        >
+          <div className="url-params-profile-page flex justify-center items-center gap-1 text-xs md:text-sm group">
+            {!isGetCateParent && categoryParents && (
+              <UrlItem data={categoryParents} />
+            )}
           </div>
-          <div className="xs:w-2/3 lg:w-1/3 mx-auto">
-            <SearchBox />
+          <div className="flex justify-center items-center ">
+            <Platform
+              data={user?.socialmedia}
+              color={`text-primary-03`}
+              colorHover={`text-white`}
+            />
           </div>
+        </div>
+        <div className="w-full xs:w-2/3 lg:w-1/3 mx-auto">
+          <SearchBox />
         </div>
       </div>
     </div>
