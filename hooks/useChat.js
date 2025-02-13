9fd5b77119e341c63c618aa520b498575f71eece
Mohammad Reza Faramarzi <mf575583@gmail.com>
2025-02-13 13:45:30 +0330

    update auth

--- hooks/useChat.js
+++ hooks/useChat.js
@@ -2,46 +2,51 @@ import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-let socket
+let socket;
 
 const useChat = () => {
   const [socketData, setSocket] = useState(null);
   const [messages, setMessages] = useState([]);
   const userToken = Cookies.get("accessToken");
-  console.log(userToken);
 
-  useEffect(() => {
-    socket = io("https://api.mahbubtarin.com:2021");
+  // useEffect(() => {
+  //   socket = io("https://api.mahbubtarin.com:2021", {
+  //     transports: ["websocket"],
+  //   });
 
-    socket.emit("authenticate", userToken);
+  //   socket.emit("authenticate", userToken);
 
-    socket.on("private-message", (data) => {
-      const decodedMessage = decodeURIComponent(escape(atob(data.message)));
-      console.log(data);
+  //   socket.on("private-message", (data) => {
+  //     const decodedMessage = decodeURIComponent(escape(atob(data.message)));
+  //     console.log(data);
 
-      setMessages((prev) => [
-        ...prev,
-        { sender: data.sender_id, text: decodedMessage },
-      ]);
-    });
-    console.log(socket);
-    
-    setSocket(socket);
+  //     setMessages((prev) => [
+  //       ...prev,
+  //       { sender: data.sender_id, text: decodedMessage },
+  //     ]);
+  //   });
+  //   console.log(socket);
 
-    return () => socket.disconnect();
-  }, [userToken]);
-//   console.log(socketData);
+  //   socket.on("connect", () => console.log("✅ سوکت متصل شد!"));
+  //   socket.on("connect_error", (err) => console.log("❌ خطای اتصال:", err));
+  //   socket.on("disconnect", () => console.log("❌ سوکت قطع شد!"));
+
+  //   setSocket(socket);
+
+  //   return () => socket.disconnect();
+  // }, [userToken]);
+  //   console.log(socketData);
 
   const sendMessage = (senderId, receiverId, text) => {
     // if (socket) {
     console.log(senderId, receiverId, text);
-    
-      const encodedMessage = btoa(unescape(encodeURIComponent(text)));
-      socket.emit("private-message", {
-        sender_id: senderId,
-        receiver_id: receiverId,
-        message: encodedMessage,
-      });
+
+    const encodedMessage = btoa(unescape(encodeURIComponent(text)));
+    socket.emit("private-message", {
+      sender_id: senderId,
+      receiver_id: receiverId,
+      message: encodedMessage,
+    });
     // }
   };
 
